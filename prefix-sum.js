c64cb6d7277c9057ce8f9e2655a3916f4c394e18
sunabhasinha <mailsunabha@gmail.com>
2022-03-23 01:11:37 +0530

    Prefix sum snippets

---
+++ prefix-sum.js
@@ -0,0 +1,13 @@
+//Prefix Sum Brute
+const prefix_brute = (arr) => {
+  const n = arr.length;
+  const result = new Array(n);
+  result[0] = arr[0];
+  for (let i = 1; i < n; i++) result[i] = result[i - 1] + arr[i];
+
+  return result;
+};
+
+//Prefix Sum Optimized
+const prefix_optimized = (arr) =>
+  arr.reduce((prev, curr, ind) => [...prev, curr + (prev[ind - 1] || 0)], []);
